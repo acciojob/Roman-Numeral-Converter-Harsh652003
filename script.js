177077e5475fc55282b90970b71cda174f18ed68
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-08-01 01:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+       0: ['M', 1000], 
+        1: ['CM', 900], 
+        2: ['D', 500], 
+        3: ['CD', 400], 
+        4: ['C', 100], 
+        5: ['XC', 90], 
+        6: ['L', 50], 
+        7: ['XL', 40], 
+        8: ['X', 10], 
+        9: ['IX', 9], 
+        10: ['V', 5], 
+        11: ['IV', 4], 
+        12: ['I', 1]
     };
 	
-
-  
     let roman = '';
-    const keys = Object.keys(obj);
-
-    for (let i = 0; i < keys.length; i++) {
-        let [letter, value] = obj[keys[i]];
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+        let [letter, value] = obj[i];
         while (num >= value) {
             roman += letter;
             num -= value;
@@ -23,6 +25,7 @@ function convertToRoman(num) {
     }
 
     return roman;
+
   //your code here
 
 }
